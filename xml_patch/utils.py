18ce4feadc246ffbc71ddfd7d96f821ebcb348b1
eidng8 <cheung.jackey@gmail.com>
2020-07-26 09:57:17 +0800

    chore

--- xml_patch/utils.py
+++ xml_patch/utils.py
@@ -5,11 +5,11 @@ def is_attribute_node(target) -> bool:
     """Check whether the given target is an attribute.
 
     Parameters
-    ----------
+    ==========
         target ([any]): Target to be checked.
 
-    Returns:
-    --------
+    Returns
+    =======
         bool: Returns `True` if target is an attribute.
     """
     return hasattr(target, 'is_attribute') and target.is_attribute
@@ -19,11 +19,11 @@ def is_text_node(target) -> bool:
     """Check whether the given target is a text node.
 
     Parameters
-    ----------
+    ==========
         target ([any]): Target to be checked.
 
-    Returns:
-    --------
+    Returns
+    =======
         bool: Returns `True` if target is a text node.
     """
     return hasattr(target, 'is_text') and target.is_text
@@ -33,11 +33,11 @@ def has_no_child_element(target) -> bool:
     """Check whether the given target doesn't have any child element.
 
     Parameters
-    ----------
+    ==========
         target ([any]): Target to be checked.
 
-    Returns:
-    --------
+    Returns
+    =======
         bool: Returns `True` if target doesn't have any child element.
     """
     return etree.iselement(target) and 0 == len(target)
